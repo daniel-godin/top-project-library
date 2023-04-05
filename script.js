6a17ab41576b05e810a731b820942cd2086d0d94
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2023-04-05 07:37:38 -0700

    manually added two books to myLibrary array.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let myLibrary = [
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R Tolkien',
+    pages: 295,
+    read: 'not read yet',
+  },
+  {
+    title: 'The Name Of The Wind',
+    author: 'Patrick Rothfuss',
+    pages: 662,
+    read: 'read',
+  }
+
+
+];
+
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
@@ -9,5 +26,9 @@ function Book(title, author, pages, read) {
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+
+function addBookToLibrary() {
+  // 
+}
 
 console.log(theHobbit.info());
